76d98436657f6991807525fbbfbabf70b7ce43fd
marcos <martinmarcospablo@gmail.com>
2024-02-12 22:32:25 -0300

    cambios varios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet,],
+  templateUrl: './app.component.html', 
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'primeraAppAngular';
-
+  
   saludo = "Hola angular";
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { EmpleadosComponent } from './empleados/empleados/empleados.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'empleados',
+        component: EmpleadosComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/AppComponent'
+    }
+];
